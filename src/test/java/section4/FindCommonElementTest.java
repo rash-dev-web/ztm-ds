6ab5a63fbe1d456bc4a6741edacf762b91857222
Rasheed Mohammad <rasheed.m@live.com>
2022-09-03 19:17:53 +0530

    add section 4 code

--- src/test/java/section4/FindCommonElementTest.java
+++ src/test/java/section4/FindCommonElementTest.java
@@ -1,11 +1,13 @@
 package section4;
 
+import java.util.HashSet;
+
 import org.testng.annotations.Test;
 
 public class FindCommonElementTest {
 
 	int[] array1 = { 1, 2, 3, 4, 5 };
-	int[] array2 = { 5, 7, 8, 9, 0 };
+	int[] array2 = { 9, 7, 8, 9, 0 };
 
 	@Test
 	public void findCommonElement() {
@@ -13,16 +15,33 @@ public class FindCommonElementTest {
 		System.out.println(commonElement(array1, array2));
 	}
 
-	public boolean commonElement(int arr1[], int arr2[]) {
-		System.out.println("array 1 lenght: " + arr1.length);
-		System.out.println("array 2 lenght: " + arr2.length);
-		boolean flag = false;
+	@Test
+	public void findElementHashSet() {
+		System.out.println(commonElementHashSet(array1, array2));
+	}
 
+	private boolean commonElementHashSet(int[] arr1, int[] arr2) {
+		HashSet<Integer> hs = new HashSet<Integer>();
+		for (int i = 0; i < arr1.length; i++) {
+			hs.add(arr1[i]);
+		}
+		
+		for(int j=0;j<arr2.length; j++) {
+			if(hs.contains(arr2[j])) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean commonElement(int arr1[], int arr2[]) {
+		boolean flag = false;
 		for (int i = 0; i < arr1.length; i++) {
 			for (int j = 0; j < arr2.length; j++) {
 				if (arr1[i] == arr2[j]) {
-					System.out.println(arr1[i] + " "+ arr2[j]);
-					flag =  true;
+					System.out.println(arr1[i] + " " + arr2[j]);
+					flag = true;
 					break;
 				}
 			}
